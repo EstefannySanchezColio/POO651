127998fcb14e1425eecc0bc7aa450806ac1bd80b
Estefanny <fsanscoli@gmail.com>
2023-09-29 11:41:16 -0600

    subi archivos

---
+++ Arreglos/ArrayListSeriesTV.java
@@ -0,0 +1,16 @@
+
+package temaarrays;
+import java.util.ArrayList;
+
+public class ArrayListSeriesTV {
+    public static void main(String[]args){
+    ArrayList<String>listaseries = new ArrayList<>();
+    listaseries.add("Braking bad");
+     listaseries.add("Friends");
+      listaseries.add("Vikings");
+      
+    System.out.println(listaseries);
+    
+    
+}
+}
